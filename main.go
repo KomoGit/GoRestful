0d7de330bb43abf69e7e87304abb016b33a9825d
James <entreur@gmail.com>
2022-10-20 13:28:55 +0400

    Created a mock DB.

--- main.go
+++ main.go
@@ -1,9 +1,70 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+// Book struct
+type Book struct {
+	ID     string  `json:"id"`
+	ISBN   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+//Fake database
+
+var books []Book
+
 func main() {
-	log.Println("Hallo!")
+	router := mux.NewRouter()
+
+	//Mock data
+	books = append(books, Book{ID: "1", ISBN: "23134", Title: "GoLang 101", Author: &Author{FirstName: "James", LastName: "Clyde"}})
+
+	router.HandleFunc("/api/books", getBooks).Methods("GET")
+	router.HandleFunc("/api/books/{id}", getBookById).Methods("GET")
+	router.HandleFunc("/api/books", createBook).Methods("POST")
+	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBookById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
 }
