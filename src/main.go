d94d312146d796598e91b0d08c498658f33ecce6
James <entreur@gmail.com>
2022-10-22 18:00:07 +0400

    Even more architecture.

--- src/main.go
+++ src/main.go
@@ -10,36 +10,31 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// Book struct
-// type Book struct {
-// 	ID     string  `json:"id"`
-// 	ISBN   string  `json:"isbn"`
-// 	Title  string  `json:"title"`+
-// 	Author *Author `json:"author"`
-// }
-
-// type Author struct {
-// 	FirstName string `json:"firstname"`
-// 	LastName  string `json:"lastname"`
-// }
-
 //Fake database
 
 var books []Book
+var r = mux.NewRouter()
 
 func main() {
-	router := mux.NewRouter()
 
 	//Mock data
 	books = append(books, Book{ID: "1", ISBN: "23134", Title: "GoLang 101", Author: &Author{FirstName: "James", LastName: "Clyde"}})
+	RouteHandlers()
+	// router.HandleFunc("/api/books", getBooks).Methods("GET")
+	// router.HandleFunc("/api/books/{id}", getBookById).Methods("GET")
+	// router.HandleFunc("/api/books", createBook).Methods("POST")
+	// router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	// router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
-	router.HandleFunc("/api/books", getBooks).Methods("GET")
-	router.HandleFunc("/api/books/{id}", getBookById).Methods("GET")
-	router.HandleFunc("/api/books", createBook).Methods("POST")
-	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+func RouteHandlers() {
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBookById).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
